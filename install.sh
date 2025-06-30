df3bd5689825d0430905fb72519a4bba2354c96e
mwmQi <rajeev.khg12@gmail.com>
2025-06-30 22:27:51 +0530

    Initial commit

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PREFIX="/data/data/com.termux/files/usr"
+
+mkdir -p $PREFIX/bin
+cp bin/mscanfocus $PREFIX/bin
+chmod +x $PREFIX/bin/mscanfocus
+
+echo "mscanfocus has been installed. You can now run it by typing 'mscanfocus' in your terminal."
